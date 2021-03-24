5c42d0353d5f17ef32403bc90a7cc4c398c74448
Alexis Varela <alexisbec13@hotmail.com>
2021-03-24 14:49:57 -0600

    create association between events and creators

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_184558) do
+ActiveRecord::Schema.define(version: 2021_03_24_202135) do
 
   create_table "events", force: :cascade do |t|
     t.string "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
