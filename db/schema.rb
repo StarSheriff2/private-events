374f0393c0d77725c56c6e8d2b536ea682435b9a
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-24 13:00:10 -0600

    create event model and create associations with User and Events created by user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_215422) do
+ActiveRecord::Schema.define(version: 2021_03_24_184558) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "date"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
