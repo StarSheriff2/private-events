8325997f5cc9a061ea92482fed1b0760fab47015
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-24 16:14:42 -0600

    create events controller and build show action and view of EventsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :events, only: [:new, :create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'users#new'
