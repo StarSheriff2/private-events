f5122b1083dd550599c601d96dcc4488b5087d23
Alexis Varela <alexisbec13@hotmail.com>
2021-03-25 09:30:59 -0600

    build form for new events and edit the create action for the events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,11 +15,12 @@ class EventsController < ApplicationController
   # GET /users/new
   def new
     @event = Event.new
+    @_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
   end
 
   # POST /users
   def create
-    @event = Event.new(event_params)
+    @event = @_current_user.created_events.build(event_params)
 
     respond_to do |format|
       if @event.save
@@ -39,7 +40,7 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :date, :location, :creator_id)
+      params.require(:event).permit(:title, :description, :date, :location, :creator_id)
     end
 
 end
