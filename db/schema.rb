cbe4a6f7ff62d9e2cfb2718212fbf85a88a2479b
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-25 16:10:00 -0600

    change date column to datetime type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_161750) do
+ActiveRecord::Schema.define(version: 2021_03_25_214027) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "attendee_id"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_25_161750) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string "date"
+    t.datetime "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
