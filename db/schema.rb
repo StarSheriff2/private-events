a115d9819e5faa806ef21b9742d0077525f2af28
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-25 16:26:54 -0600

    change datetime column in events to date and edit show page for Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_214027) do
+ActiveRecord::Schema.define(version: 2021_03_25_221845) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "attendee_id"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_25_214027) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.datetime "date"
+    t.date "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
