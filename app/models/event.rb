3c548c49fd8ff6cb9d8487f1262cdf3fb99dda1f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-25 11:29:24 -0600

    create migration and associations for Attendances

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :attendance, source: :attendee_id
 end
