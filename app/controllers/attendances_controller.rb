e29b6a3818511c9ce6251c11461bb121a18b2591
Alexis Varela <alexisbec13@hotmail.com>
2021-03-26 13:05:58 -0600

    add register action to events table

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,13 @@
+class AttendancesController < ApplicationController
+  def create
+    @attendance = Attendance.new(attended_event_id: params[:attended_event_id], attendee_id: params[:attendee_id])
+
+    respond_to do |format|
+      if @attendance.save
+        format.html { redirect_to event_path(@attendance.attended_event_id), notice: "You registered succesfully to this event." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :events, only: [:new, :create, :show, :index]
+  resources :attendances, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'events#index'
