16b3afbc6d717e60cf733a0901330a503dec6ad3
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-26 13:21:25 -0600

    add validations to User and Event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,12 @@ class Event < ApplicationRecord
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances
 
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :date, presence: true
+  validates :description, presence: true, length: { minimum: 5 }
+  validates :location, presence: true, length: { minimum: 5 }
+
+
   scope :past, -> { where('date < ?', DateTime.current.localtime) }
   scope :upcoming, -> { where('date > ?', DateTime.current.localtime) }
 end
