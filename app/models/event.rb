f3cd09bc18e141f5a3190b6b0d32a67852f63c6c
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-26 09:57:34 -0600

    add scope method for past and upcoming events on the event model, and add New Event button to Navbar

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances
+
+  scope :past, -> { where('date < ?', DateTime.current.localtime) }
+  scope :upcoming, -> { where('date > ?', DateTime.current.localtime) }
 end
