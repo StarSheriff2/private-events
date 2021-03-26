599c2dadc73f12c5244108ecbe01b45af5deb4a6
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-26 16:28:47 -0600

    create test units for Event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,12 +1,31 @@
 require 'rails_helper'
 
 describe Event, :type => :model do
+  # Model Tests
+  context "check CRUD actions work" do
+    before do
+      @event = Event.create(title: 'My_event', description: 'description', location: 'location', date: DateTime.now, creator: User.new)
+    end
+
+    it 'checks that an event can be created' do
+      expect(@event).to be_valid
+    end
+
+    it 'checks that a event can be read' do
+      expect(Event.find_by_title("My_event")).to eq(@event)
+    end
+  end
+
   # Association Tests
   it { should have_many(:attendances) }
   it { should have_many(:attendees) }
   it { should belong_to(:creator) }
 
   # Validation Tests
+  it "is not valid without valid attributes" do
+    expect(Event.new).to_not be_valid
+  end
+
   describe '#title' do
     it { should validate_presence_of(:title) }
   end
